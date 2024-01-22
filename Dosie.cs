9b6f4308cc523a5d06181aaa27be1d987402e729
sashasuro <123554714+sashasuro@users.noreply.github.com>
2024-01-22 03:07:05 +0300

    Add files via upload

---
+++ Dosie.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _19._01.Dosie;
+
+namespace _19._01
+{
+    class Dosie
+    {
+        public class Student
+        {
+            private string name;
+            private string gender;
+            private int year;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public string Gender
+            {
+                get { return gender; }
+                set
+                {
+                    int male = 0;
+                    int fem = 0;
+
+                    foreach (Student student in gender)
+                    {
+                        if (student.Gender.ToLower() == "male")
+                        {
+                            male++;
+                        }
+                        else if (student.Gender.ToLower() == "female")
+                        {
+                            fem++;
+                        }
+                    }
+                }
+            }
+
+            public int Year
+            {
+                get { return year; }
+                set{ year = value; }
+            }
+
+            public Student(string name, string gender, int year)
+            {
+                Name = name;
+                Gender = gender;
+                Year = year;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _19._01.Dosie;
+
+namespace _19._01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 3;
+            Student students = new Student;
+
+            students = new Student("Егор Дуба", "мужчина", 2006);
+            students1 = new Student("Щуровская Александра", "девушка", 2006);
+            students2 = new Student("Виктория Шурдук", "девушка", 2006);
+        }
+    }
+}
